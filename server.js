2a26702f2ba8e2edf44092f2c26a3f85c244e4e8
aashishvikram <aashishvikram@ccs.neu.edu>
2017-05-25 23:40:18 -0400

    website-builder index edit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 
 
 // configure a public directory to host static content
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/assignment'));
 
 require ("./test/app.js")(app);
 require("./assignment/app.js")(app);
